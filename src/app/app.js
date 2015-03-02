f2d422ef7065982e4f5ad53af9381f701499ea73
arshadsplendo <arshad@splendo.nl>
2015-03-02 11:12:22 +0100

    #4 Experimenting with general layout

--- src/app/app.js
+++ src/app/app.js
@@ -6,12 +6,32 @@ angular.module('Tacx', [
     'ngMaterial'
 ])
 
-.controller('AppCtrl', function($scope, $timeout, $mdSidenav, $log) {
+.controller('AppCtrl', function($scope, $timeout, $mdSidenav, $log, $mdToast) {
   $scope.toggleLeft = function() {
     $mdSidenav('left').toggle()
                       .then(function(){
                           $log.debug("toggle left is done");
                       });
+  };
+
+  $scope.toastPosition = {
+    bottom: false,
+    top: true,
+    left: false,
+    right: true
+  };
+  $scope.getToastPosition = function() {
+    return Object.keys($scope.toastPosition)
+      .filter(function(pos) { return $scope.toastPosition[pos]; })
+      .join(' ');
+  };
+  $scope.showSimpleToast = function() {
+    $mdToast.show(
+      $mdToast.simple()
+        .content('Simple Toast!')
+        .position($scope.getToastPosition())
+        .hideDelay(3000)
+    );
   };
 })
 .controller('LeftCtrl', function($scope, $timeout, $mdSidenav, $log) {
@@ -22,6 +42,10 @@ angular.module('Tacx', [
                       });
   };
 })
-
+.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('pink')
+    .accentPalette('orange');
+})
 
 ;
